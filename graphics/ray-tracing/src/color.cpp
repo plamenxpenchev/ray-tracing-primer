5e57d77a9a66bd857414360ccfac7db7ec611efd
Plamen Penchev <plamenxpenchev@gmail.com>
2023-02-18 10:57:10 +0200

    Whitespace formatting

--- graphics/ray-tracing/src/color.cpp
+++ graphics/ray-tracing/src/color.cpp
@@ -1,6 +1,7 @@
 #include "color.hpp"
 
 void write_color(std::ostream& out, color pixel_color) {
+
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
--- graphics/ray-tracing/src/ray.cpp
+++ graphics/ray-tracing/src/ray.cpp
@@ -1,11 +1,16 @@
 #include "ray.hpp"
 
 point3 ray::origin() const {
+
 	return orig;
 }
+
 vec3 ray::direction() const {
+
 	return dir;
 }
+
 point3 ray::at(double t) const {
+
 	return orig + t * dir;
 }
