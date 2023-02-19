a9ac0bcda599ea5d6a38e6010a4a4f3f2978ee57
Plamen Penchev <plamenxpenchev@gmail.com>
2023-02-19 10:34:17 +0200

    Tie the new classes together in the implementation in main

--- graphics/ray-tracing/src/main.cpp
+++ graphics/ray-tracing/src/main.cpp
@@ -1,59 +1,21 @@
 #include <iostream>
 
-#include "ray.hpp"
-#include "vec3.hpp"
+#include "rtweekend.hpp"
+
 #include "color.hpp"
+#include "hittable_list.hpp"
+#include "sphere.hpp"
 
-/*
-	For a point P = [x, y, z] of a vector to lie on a sphere with center C = [Cx, Cy, Cz] and radius r,
-	it must satisfy (x - Cx)^2 + (y - Cy)^2 + (z - Cz)^2 = r^2.
-	In vector notation we can write that as (P-C) * (P-C) = r^2.
+color ray_color(const ray& r, const hittable& world) {
 
-	For a ray, defined as P(t) = A + tb, to hit the sphere it must satisfy the equation (P(t)-C) * (P(t)-C) = r^2.
-	t^2(b * b) + 2t(b * (A-C)) + (A-C) * (A-C) - r^2 = 0.
-	The vectors and r are constants and known, thus we can solve the quadratic for t, 
-	and determine if the ray intersects the sphere at two points (two roots, discriminant > 0).
-*/
-double hit_sphere(const point3& center, double radius, const ray& r) {
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec)) {
 
-	vec3 oc = r.origin() - center;
-
-	/*
-		For any vector V = [v1, v2, ...] its length is ||V|| = sqrt(v1*v1 + v2*v2 + ...).
-		Naturally, V*V = ||V||^2
-	*/
-	auto a = r.direction().length_squared(); // equivalent to a = dot(r.direction(), r.direction());
-	auto half_b = dot(oc, r.direction()); // where b = 2.0 * dot(oc, r.direction());
-	auto c = oc.length_squared() - radius * radius; // equivalent to c = dot(oc, oc) - radius * radius
-
-	/*
-		Using half_b, we can extract the 4 from the discriminant square root term
-		in determining the roots. Also then we reduce the 2 from the fraction to further simplify.
-	*/
-	auto discriminant = half_b * half_b - a * c;
-	if (discriminant < 0) {
-		return -1.0;
-	}
-	else {
-		// Use the closest hit point, the smaller root
-		return (-half_b - sqrt(discriminant)) / a;
-	}
-}
-
-color ray_color(const ray& r) {
-
-	// Sphere with center [0, 0, -1] and radius 0.5
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-	if (t > 0.0) {
-
-		// For a sphere the outward surface normal vector 
-		// is in the direction of the hit point minus the center.
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
-	t = 0.5 * (unit_direction.y() + 1.0);
+	auto t = 0.5 * (unit_direction.y() + 1.0);
 
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
@@ -64,6 +26,11 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+	// World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto viewport_height = 2.0;
@@ -86,8 +53,8 @@ int main() {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
 
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+			color pixel_color = ray_color(r, world);
 
             write_color(std::cout, pixel_color);
 		}
